1483ef10924311f8f2d5b04250c9d4099ca4e451
Devvrit <f20150053@goa.bits-pilani.ac.in>
2022-03-12 17:29:43 +0530

    .

--- compare_covariance.py
+++ compare_covariance.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 torch.set_printoptions(threshold=300)
 
 S = torch.tensor(scipy.io.loadmat('cov.mat')['S'])
+# S = torch.tensor(np.load("../test_mat_2.pickle", allow_pickle=True))
 X = torch.load("X_quic.pt")
 print("S size: " + str(S.size()))
+print("X size: " + str(X.size()))
 print("total number of entries in S: " + str(S.size(0)**2))
 print("num non-zero entries in X: " + str(X.size(0)**2-(X==0).sum()))
 
@@ -35,13 +37,14 @@ if X.size(0)<3:
 	print("Sinv: " + str(Sinv))
 	print("S: " + str(S))
 #Calculate logDetDiverLoss:
-lamb=0.5
-# torch.trace(torch.matmul(S,X))
-# -torch.log(torch.det(X))
-diag = [i for i in range(X.size(0))]
-print("(torch.sign(X.view(-1))*X.view(-1)): " + str((torch.sign(X[diag,diag].view(-1))*X[diag,diag].view(-1))))
+lamb=0.005
+
 loss = -torch.log(torch.det(X.double()))+torch.trace(torch.matmul(S.double(),X.double())) + lamb*(torch.sign(X.view(-1))*X.view(-1)).sum()
-print("loss: " + str(loss))
+print("quic loss: " + str(loss))
+loss = -torch.log(torch.det(torch.matmul(X.double(),S.double())))+torch.trace(torch.matmul(S.double(),X.double())) - X.size(0)
+print("LogDetDiv: " + str(loss))
+print("Frob norm of (X-S^{-1}): " + str(torch.norm(X-Sinv)))
+print("XS-I frob norm: " + str(torch.norm(torch.matmul(X.double(),S.double())-torch.eye(X.size(0)))))
 
 plt.plot([i for i in range(len(evalueX))], torch.log(torch.tensor(evalue_invS)), marker=".", markersize=5)
 # plt.xlabel("eigenvalues")
